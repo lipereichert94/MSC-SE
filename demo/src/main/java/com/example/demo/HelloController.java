0fc3c2be32e3307a58d1bee6c37c049e0467f470
lipereichert94 <56693315+lipereichert94@users.noreply.github.com>
2024-06-10 21:16:47 -0500

    eee

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot22eeeeee!";
+		return "Greetings from Spring Boot22eeeeeef!";
 	}
 
 }
